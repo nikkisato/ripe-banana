35afa884eed5f764c02e29bedad15e246d4e524b
Nikki <nikki.satopdx@gmail.com>
2020-01-06 14:21:33 -0800

    got one actor routes to pass

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 
 const request = require('supertest');
 const app = require('../lib/app');
-const connect = require('../lib/utils/connect');
+const connect = require('../lib/utils/connect')
+;
+const Actor = require('../lib/models/Actor');
 const mongoose = require('mongoose');
 
 describe('app routes', () => {
@@ -10,6 +12,7 @@ describe('app routes', () => {
     connect();
   });
 
+  let actor;
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
@@ -17,4 +20,46 @@ describe('app routes', () => {
   afterAll(() => {
     return mongoose.connection.close();
   });
+
+  //actor routes
+  it('can create a get actors names', () => {
+    return request(app)
+      .get('/actors')
+      .send({
+        name:'Corgi Godzilla'
+      })
+      .then(res => {
+        expect(res.body).toEqual[{
+          _id: expect.any(String),
+          name:'Corgi Godzilla',
+          __v: 0
+        }];
+      });
+  });
+
+  it('can create a get actors names by ID', () => {
+    return request(app)
+      .get(`/actors/${actor.id}`)
+      .send({
+        name:'Corgi Godzilla'
+      })
+      .then(res => {
+        expect(res.body).toEqual[{
+          _id: expect.any(String),
+          name:'Corgi Godzilla',
+          __v: 0
+        }];
+      });
+  });
+
+  //Studio routes
+
+
+
+
+
+
+
+
+
 });
