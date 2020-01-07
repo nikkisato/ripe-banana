ee2a7fe99593117814534d674f38f62a233c6113
Nikki <nikki.satopdx@gmail.com>
2020-01-07 09:29:20 -0800

    updated routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,6 +6,8 @@ const connect = require('../lib/utils/connect')
 ;
 const Actor = require('../lib/models/Actor');
 const Studio = require('../lib/models/Studio');
+const Film = require('../lib/models/Film');
+const Reviewer = require('../lib/models/Reviewer');
 const mongoose = require('mongoose');
 
 describe('app routes', () => {
@@ -24,24 +26,37 @@ describe('app routes', () => {
   beforeEach(async() => {
     actor = await Actor
       .create({
-        name:'Corgi Godzilla',
-        dob: '1954-04-19',
-        pob: 'Tokyo Japan',
         films: [{
+          id:'1234',
           title: 'Corgi Land',
           released: 1964 
-        }]
+        }],
+        name:'Corgi Godzilla',
+        dob: '1954-04-19',
+        pob: 'Tokyo Japan'
       });
 
     studio = await Studio
       .create({
-        name:'Studio Ghibli'
+        name:'Studio Ghibli',
+        address:'1 Chome-1-83 Shimorenjaku, Mitaka, Tokyo 181-0013, Japan',
+        films: [{
+          _id:'1234',
+          title: 'Godzilla'
+        }],
+      });
+
+    reviewer = await Reviewer 
+      .create({
+        name:'Hayao Miyazaki',
+        company: 'Studio Ghibli'
       });
   });
 
   afterAll(() => {
     return mongoose.connection.close();
   });
+
 
   //actor routes
   it('can get all actors names', () => {
@@ -63,7 +78,7 @@ describe('app routes', () => {
         expect(res.body).toEqual({
           _id: actor._id.toString(),
           name:'Corgi Godzilla',
-          dob: '1954-04-19T08:00:00.000Z',
+          dob: '1954-04-19T00:00:00.000Z',
           pob: 'Tokyo Japan',
           __v: 0,
           films: [{
@@ -79,9 +94,6 @@ describe('app routes', () => {
   it('can create a get studios names', () => {
     return request(app)
       .get('/studios')
-      .send({
-        name:'Studio Ghibli'
-      })
       .then(res => {
         expect(res.body).toEqual[{
           _id: expect.any(String),
@@ -95,14 +107,6 @@ describe('app routes', () => {
   it('can create a get studios by id', () => {
     return request(app)
       .get(`/studios/${studio._id}`)
-      .send({
-        name:'Studio Ghibli',
-        address:'1 Chome-1-83 Shimorenjaku, Mitaka, Tokyo 181-0013, Japan',
-        films: [{
-          _id:'1234',
-          title: 'Godzilla'
-        }],
-      })
       .then(res => {
         expect(res.body).toEqual[{
           _id: expect.any(String),
@@ -121,10 +125,6 @@ describe('app routes', () => {
   it('can create a get reviewers', () => {
     return request(app)
       .get('/reviewer')
-      .send({
-        name:'Hayao Miyazaki',
-        company: 'Studio Ghibli'
-      })
       .then(res => {
         expect(res.body).toEqual[{
           _id: expect.any(String),
@@ -135,23 +135,19 @@ describe('app routes', () => {
       });
   });
 
-  //need to have review
-  // it('can create a get reviewers by id', () => {
-  //   return request(app)
-  //     .get('/reviewer')
-  //     .send({
-  //       name:'Hayao Miyazaki',
-  //       company: 'Studio Ghibli'
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual[{
-  //         _id: expect.any(String),
-  //         name:'Hayao Miyazaki',
-  //         company: 'Studio Ghibli',
-  //         __v: 0
-  //       }];
-  //     });
-  // });
+
+  it('can create a get reviewers by id', () => {
+    return request(app)
+      .get('/reviewer')
+      .then(res => {
+        expect(res.body).toEqual[{
+          _id: expect.any(String),
+          name:'Hayao Miyazaki',
+          company: 'Studio Ghibli',
+          __v: 0
+        }];
+      });
+  });
 
   
 });
